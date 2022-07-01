240048e933c3c594f30dadbd104ed5c0e06b96d6
Peesh98 <timothy.t.le98@gmail.com>
2022-07-01 22:08:22 +0100

    Fixed enter button functionality on search page

--- server/client/assets/index.js
+++ server/client/assets/index.js
@@ -1,21 +1,30 @@
 const searchInput = document.getElementById("searchinput");
-searchInput.addEventListener('keyup', (e) => {
-    if (e.code === "Enter") {
+
+searchInput.addEventListener('keydown', e => {
+    if(e.key == "Enter"){
         e.preventDefault();
-        document.querySelector('form').submit();
+        if (searchtext.value.trim() && searchtext.value.trim() !== undefined) {
+            const searchParams = new URLSearchParams ({
+                q: searchtext.value.trim()
+            });
+            window.location.href = `${window.location.origin}/search?${searchParams.toString()}`;
+        };
     };
 });
+
 searchInput.addEventListener('submit', search);
 
 function search(e) {
     e.preventDefault();
-    if (e.target.searchtext.value.trim() && e.target.searchtext.value.trim() !== undefined) {
+    const query = searchtext.value;
+    if (searchtext.value.trim() && searchtext.value.trim() !== undefined) {
         const searchParams = new URLSearchParams ({
-            q: e.target.searchtext.value.trim()
+            q: searchtext.value.trim()
         });
 
         if (e.submitter.name === "search") {
             window.location.href = `${window.location.origin}/search?${searchParams.toString()}`;
+            document.getElementById("searchtext").textContent = query;
         } else {
             fetch(`${window.location.origin}/search/results?${searchParams.toString()}`)
             .then(res => res.json())
